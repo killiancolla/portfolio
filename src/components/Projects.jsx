c46e060e5d82e0e96b10798afd81e68e17f1f42b
Killian Colla <k-colla@unicancer.fr>
2024-05-03 18:11:04 +0200

    projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,70 +1,53 @@
 import '../styles/Projects.css'
 import { useState } from 'react';
+import React from 'react';
 import project1 from '../assets/project1.png'
+import project2 from '../assets/project2.jpg'
+import image from '../assets/pomodoro.png'
 
 function Portfolio() {
 
-    const projectsCards = [
+    const projects = [
         {
-            title: 'Unicancer',
-            description: 'FullStack Developer',
-            link: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam doloremque deleniti nemo beatae debitis suscipit sapiente dolorum voluptatum. Incidunt, possimus illum iure ducimus natus eum pariatur repudiandae eaque dicta fuga.',
-            image: project1
+            title: 'Pomodoro',
+            image: project1,
+            techno: ["ReactJS", "PHP"]
         },
         {
-            title: 'Unicancer',
-            description: 'FullStack Developer',
-            link: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam doloremque deleniti nemo beatae debitis suscipit sapiente dolorum voluptatum. Incidunt, possimus illum iure ducimus natus eum pariatur repudiandae eaque dicta fuga.',
-            image: project1
-        },
-        {
-            title: 'Unicancer',
-            description: 'FullStack Developer',
-            link: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam doloremque deleniti nemo beatae debitis suscipit sapiente dolorum voluptatum. Incidunt, possimus illum iure ducimus natus eum pariatur repudiandae eaque dicta fuga.',
-            image: project1
-        },
-        {
-            title: 'Unicancer',
-            description: 'FullStack Developer',
-            link: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam doloremque deleniti nemo beatae debitis suscipit sapiente dolorum voluptatum. Incidunt, possimus illum iure ducimus natus eum pariatur repudiandae eaque dicta fuga.',
-            image: project1
-        },
-        {
-            title: 'Unicancer',
-            description: 'FullStack Developer',
-            link: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam doloremque deleniti nemo beatae debitis suscipit sapiente dolorum voluptatum. Incidunt, possimus illum iure ducimus natus eum pariatur repudiandae eaque dicta fuga.',
-            image: project1
-        },
-        {
-            title: 'Unicancer',
-            description: 'FullStack Developer',
-            link: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam doloremque deleniti nemo beatae debitis suscipit sapiente dolorum voluptatum. Incidunt, possimus illum iure ducimus natus eum pariatur repudiandae eaque dicta fuga.',
-            image: project1
-        },
-        {
-            title: 'Unicancer',
-            description: 'FullStack Developer',
-            link: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam doloremque deleniti nemo beatae debitis suscipit sapiente dolorum voluptatum. Incidunt, possimus illum iure ducimus natus eum pariatur repudiandae eaque dicta fuga.',
-            image: project1
-        },
-        {
-            title: 'Unicancer',
-            description: 'FullStack Developer',
-            link: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam doloremque deleniti nemo beatae debitis suscipit sapiente dolorum voluptatum. Incidunt, possimus illum iure ducimus natus eum pariatur repudiandae eaque dicta fuga.',
-            image: project1
-        },
-        {
-            title: 'Unicancer',
-            description: 'FullStack Developer',
-            link: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam doloremque deleniti nemo beatae debitis suscipit sapiente dolorum voluptatum. Incidunt, possimus illum iure ducimus natus eum pariatur repudiandae eaque dicta fuga.',
-            image: project1
-        },
+            title: 'tructruc',
+            image: project2,
+            techno: ["ReactJS", "PHP"]
+        }
     ];
+
+    const columnCount = 3;
+    const projectsPerColumn = Math.ceil(projects.length / columnCount);
+
+    const columns = [];
+
+    for (let i = 0; i < columnCount; i++) {
+        const start = i * projectsPerColumn;
+        const end = start + projectsPerColumn;
+        columns.push(projects.slice(start, end));
+    }
 
     const [activeItem, setActiveItem] = useState('all');
 
     const handleClick = (itemName) => {
         setActiveItem(itemName);
+    };
+
+    const [processing, setProcessing] = useState([]);
+
+    const handleButtonClick = (index) => {
+        setProcessing((prev) => [...prev, index]);
+        setTimeout(() => {
+            setProcessing((prev) => prev.filter((item) => item !== index));
+        }, 2000); // Réinitialiser après 2 secondes
+    };
+
+    const handleRestartClick = () => {
+        setProcessing([]);
     };
 
     return (
@@ -80,21 +63,38 @@ function Portfolio() {
                 <li className={activeItem === 'php' ? 'active' : ''} onClick={() => setActiveItem('php')}>PHP</li>
                 <li className={activeItem === 'python' ? 'active' : ''} onClick={() => setActiveItem('python')}>Python</li>
             </ul>
-            {projectsCards.reduce((acc, _, index, array) => {
-                if (index % 5 === 0) {
-                    acc.push(array.slice(index, index + 5));
-                    console.log(acc);
-                }
-                return acc;
-            }, []).map((subArray, index) => (
-                <div key={index} className='projectsGrid'>
-                    {subArray.map((work, i) => (
-                        <div key={i} className={`p p${(i % 5) + 1}`} style={{ backgroundImage: `url(${work.image})` }}>
-                            <div className="footer"></div>
-                        </div>
-                    ))}
-                </div>
-            ))}
+            <div className="grid-3">
+                {columns.map((column, index) => (
+                    <div key={index} className="column">
+                        {column.map((project, index) => (
+                            <div key={index} className="card">
+                                <div className="img">
+                                    <img src={project.image} alt={project.title} />
+                                    <button
+                        key={index}
+                        className={`button ${processing.includes(index) ? 'processing' : ''}`}
+                        onClick={() => handleButtonClick(index)}
+                    >
+                        <span>Submit</span>
+                        <svg viewBox="0 0 15 13">
+                            <polyline points="2 6.5 6 10.5 13 2.5"></polyline>
+                        </svg>
+                    </button>
+                                </div>
+                                <h2>{project.title}</h2>
+                                <h3>
+                                {project.techno.map((tech, index) => (
+                                    <React.Fragment key={index}>
+                                        {index > 0 && ' • '}
+                                        {tech}
+                                    </React.Fragment>
+                                ))}
+                            </h3>
+                            </div>
+                        ))}
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
