4068fd62e58661ca06a6367cdc6032582fa98e89
killiancolla <killiancolla@hotmail.fr>
2024-10-28 00:49:39 +0100

    test

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -28,6 +28,7 @@ export default function HomePage() {
         >
           {t('hello')}
         </span>
+        <p>Coucou</p>
         <h2 className="font-bold text-5xl">{t('title')}</h2>
         <p className='w-1/2 text-center'>
           {t('intro')}
