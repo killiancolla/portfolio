9d0aa91d5c3bb7edace8769c83676d91f54c22a1
killiancolla <killiancolla@hotmail.fr>
2025-02-11 17:52:13 +0100

    update cv

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -32,7 +32,7 @@ export default function About() {
                 </h3>
             </div>
             <p className="sm:w-2/5 max-sm:w-5/6 italic font-thin leading-7 mb-5">{t("about_desc")}</p>
-            <a href="CV_2024-12-01_Killian_Colla.pdf" target="_blank" rel="noopener noreferrer">
+            <a href="CV_FR_Killian_Colla.pdf" target="_blank" rel="noopener noreferrer">
                 <Button aria-label="Download CV" className="">{t("cv-download")}<ArrowDownFromLine className="py-1" /></Button>
             </a>
         </motion.div>
